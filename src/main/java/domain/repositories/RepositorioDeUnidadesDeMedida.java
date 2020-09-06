b69d1ca069ca65cbc183c4984f7759fe1a534a31
CSegundoRosa <69321526+CSegundoRosa@users.noreply.github.com>
2020-09-06 19:55:26 -0300

    Unidades de medida
    +Equivalencias

--- src/main/java/domain/repositories/RepositorioDeUnidadesDeMedida.java
+++ src/main/java/domain/repositories/RepositorioDeUnidadesDeMedida.java
@@ -15,8 +15,26 @@ public class RepositorioDeUnidadesDeMedida {
         UnidadDeMedida metro = new UnidadDeMedida("Metro");
         UnidadDeMedida kilometro = new UnidadDeMedida("Kilometro");
 
+        milimetro.agregarEquivalencia(milimetro, 1.00);
+        milimetro.agregarEquivalencia(centimetro,0.1);
+        milimetro.agregarEquivalencia(metro, 0.001);
+        milimetro.agregarEquivalencia(kilometro,0.000001);
+
         centimetro.agregarEquivalencia(metro,0.01);
-        //agregar el resto
+        centimetro.agregarEquivalencia(centimetro, 1.00);
+        centimetro.agregarEquivalencia(milimetro, 10.00);
+        centimetro.agregarEquivalencia(kilometro, 0.00001);
+
+        metro.agregarEquivalencia(metro, 1.00);
+        metro.agregarEquivalencia(centimetro, 100.00);
+        metro.agregarEquivalencia(milimetro, 1000.00);
+        metro.agregarEquivalencia(kilometro, 0.001);
+
+        kilometro.agregarEquivalencia(kilometro,1.00);
+        kilometro.agregarEquivalencia(metro, 1000.00);
+        kilometro.agregarEquivalencia(centimetro, 100000.00);
+        kilometro.agregarEquivalencia(milimetro, 1000000.00);
+        //agregar el resto (son esas?)
 
         this.unidadDeMedidas.add(centimetro);
         this.unidadDeMedidas.add(milimetro);
